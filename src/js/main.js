7a04cbd5471cbe7e17e8b35b5fee0eb4b5be0426
Dimitar Danov <dimitardanov@users.noreply.github.com>
2016-10-31 15:29:36 +0200

    Added openWeatherMap API call, with key read from the url as a query string '/?owm=...'

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,48 @@
 
-// var templates = require('./compiledTemplates/templates');
-//var HBRS = require('handlebars');
-
-
 $(function() {
-  console.log('hello world');
+  var $article = $('#weather-today');
+  var location = {
+    lon: 135,
+    lat: 45
+  };
+  var openWeatherMapURL = 'http://api.openweathermap.org/data/2.5/weather?';
+  var openWeatherMapAPIKey = window.location.search.slice(5);
+
+
+  var getWeather = function () {
+    $.ajax({
+      url: openWeatherMapURL,
+      data: {
+        lon: location.lon,
+        lat: location.lat,
+        APPID: openWeatherMapAPIKey
+      },
+      method: 'GET',
+      crossDomain: true,
+      jsonp: false,
+      dataType: 'json',
+      success: function (data, status, jqxhr) {
+        console.log(data);
+        console.log(status);
+      },
+      error: function (jqxhr, status, error) {
+        console.log(jqxhr);
+        console.log(status);
+        console.log(error);
+      },
+      complete: function (jqxhr, status) {
+        console.log(jqxhr);
+        console.log(status);
+      }
+    });
+  };
+
+
+  if (openWeatherMapAPIKey.length>0) {
+    getWeather();
+  } else {
+    $article.html($('<div></div>', {'class': 'alert alert-warning text-center'}).text('The sun will shine this night, expect a full moon at noon ').append($('<strong></strong>').text(':)')));
+  }
+
+
 });
