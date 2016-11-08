839b620f4c1dcaf58a0da93057442d035c6e6990
Dimitar Danov <dimitardanov@users.noreply.github.com>
2016-11-08 18:20:20 +0200

    Added licenses data

--- src/js/main.js
+++ src/js/main.js
@@ -330,6 +330,18 @@ $(function() {
     return qObj;
   };
 
+  var licenses = {
+    0: {abbr: 'All Rights Reserved', name: 'All Rights Reserved', url: '#'},
+    1: {abbr: 'CC BY-NC-SA 2.0', name: 'Attribution-NonCommercial-ShareAlike License', url: 'http://creativecommons.org/licenses/by-nc-sa/2.0/'},
+    2: {abbr: 'CC BY-NC 2.0', name: 'Attribution-NonCommercial License', url: 'http://creativecommons.org/licenses/by-nc/2.0/'},
+    3: {abbr: 'CC BY-NC-ND 2.0', name: 'Attribution-NonCommercial-NoDerivs License', url: 'http://creativecommons.org/licenses/by-nc-nd/2.0/'},
+    4: {abbr: 'CC BY 2.0', name: 'Attribution License', url: 'http://creativecommons.org/licenses/by/2.0/'},
+    5: {abbr: 'CC BY-SA 2.0', name: 'Attribution-ShareAlike License', url: 'http://creativecommons.org/licenses/by-sa/2.0/'},
+    6: {abbr: 'CC BY-ND 2.0', name: 'Attribution-NoDerivs License', url: 'http://creativecommons.org/licenses/by-nd/2.0/'},
+    7: {abbr: 'No known copyright restrictions', name: 'No known copyright restrictions', url: 'http://flickr.com/commons/usage/'},
+    8: {abbr: 'U.S. Government Works', name: 'United States Government Work', url: 'http://www.usa.gov/copyright.shtml'}
+  };
+
   queryStr = parseQueryStr();
   openWeatherMapAPIKey = queryStr.owm;
   flickrQueryData.api_key = queryStr.api_key;
