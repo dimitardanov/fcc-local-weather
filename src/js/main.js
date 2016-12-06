f11526aa57214518311d269ff2917369b432a171
Dimitar Danov <dimitardanov@users.noreply.github.com>
2016-12-06 21:57:00 +0200

    Refactoring main.js

--- src/js/main.js
+++ src/js/main.js
@@ -4,21 +4,14 @@ $(function() {
 
   var flickrOpts = require('./lib/options/flickr.js');
   var owm = require('./lib/options/openWeatherMap.js');
-  var flickrHelpers = require('./lib/helpers/flickr.js');
-  var owmHelpers = require('./lib/helpers/openWeatherMap.js');
-  var creditsRenderer = require('./lib/renderers/credits.js');
-  var bgPhoto = require('./lib/renderers/bgPhoto.js');
-  var weatherReport = require('./lib/renderers/weatherReport.js');
   var errorMsg = require('./lib/renderers/errorMessages.js');
   var helpers = require('./lib/helpers/helpers.js');
   var events = require('./lib/helpers/events.js');
   var weatherAjax = require('./lib/ajax/openWeatherMap.js');
   var flickrAjax = require('./lib/ajax/flickr.js');
 
-
-
-
   var queryObj = helpers.parseQueryStr();
+
   owm.setAPIKey(queryObj.owm);
   flickrOpts.setAPIKey(queryObj.api_key);
 
@@ -32,7 +25,6 @@ $(function() {
   } else {
     errorMsg.showLocationUnavailable();
   }
-
 
   $('#weather-today').on('click', '#c-btn, #f-btn', events.c2fButtonToggle);
 
