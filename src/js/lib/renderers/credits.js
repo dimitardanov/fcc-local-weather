37c7c8be5042b9072b46e972bb95881ad2808401
Dimitar Danov <dimitardanov@users.noreply.github.com>
2016-12-10 01:26:08 +0200

    Changed credits to use hbs template

--- src/js/lib/renderers/credits.js
+++ src/js/lib/renderers/credits.js
@@ -1,34 +1,20 @@
 
+var credits = require('../templates/credits.hbs');
+var $footer = $('footer');
 
-function _setElementCredits (selector, text, url) {
-  var $element = $(selector);
-  $element.text(text);
-  $element.attr('href', url);
-}
-
-function _setImageAuthorCredits (image) {
-  var owner_name = image.getImageOwnerName();
-  var owner_url = image.getImageOwnerURL();
-  _setElementCredits('.photo-author', owner_name, owner_url);
-}
-
-function _setImageTitleCredits (image) {
-  var photo_title = image.getImageTitle();
-  var photo_url = image.getFlickrImagePageURL();
-  _setElementCredits('.photo-title', photo_title, photo_url);
-}
-
-function _setImageLicenseCredits (image) {
-  var licenseUrl = image.getImageLicenseURL();
-  var licenseText = image.getImageLicenseText();
-  _setElementCredits('.photo-license', licenseText, licenseUrl);
-}
 
 function setImageCredits (image) {
-  _setImageAuthorCredits(image);
-  _setImageTitleCredits(image);
-  _setImageLicenseCredits(image);
+  var data = {
+    imageUrl: image.getFlickrImagePageURL(),
+    imageTitle: image.getImageTitle(),
+    ownerName: image.getImageOwnerName(),
+    ownerUrl: image.getImageOwnerURL(),
+    licenseText: image.getImageLicenseText(),
+    licenseUrl: image.getImageLicenseURL()
+  };
+  $footer.append(credits(data));
 }
+
 
 module.exports = {
   setImageCredits: setImageCredits
