fbafa080edaa78887258f9147b7ddf09aed9a549
Dimitar Danov <dimitardanov@users.noreply.github.com>
2016-12-10 02:12:45 +0200

    Changed error messages to use hbs templates

--- src/js/lib/renderers/errorMessages.js
+++ src/js/lib/renderers/errorMessages.js
@@ -1,19 +1,15 @@
 
-
+var weatherUnavailable = require('../templates/errorMessageWeatherUnavailable.hbs');
+var locationUnavailable = require('../templates/errorMessageLocationUnavailable.hbs');
 var $article = $('#weather-today');
 
 
 function showLocationUnavailable () {
-  var $alert = $('<div></div>', {'class': 'alert alert-warning text-center'});
-  var $joke = $('<p></p>').text('I guess the sun will shine tonight, expect a full moon at noon ').append($('<strong></strong>').text(':)'));
-  var $msg = $('<p></p>').text('Location, location, location, are you under cover?');
-  $alert.append($msg);
-  $alert.append($joke);
-  $article.html($alert);
+  $article.html(locationUnavailable());
 }
 
 function showWeatherUnavailable () {
-  $article.html($('<div></div>', {'class': 'alert alert-warning'}).text('Can\'t fetch the weather, plase try again.'));
+  $article.html(weatherUnavailable());
 }
 
 
