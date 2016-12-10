92219df00f2c376d90a7d8386f6bded6a0750649
Dimitar Danov <dimitardanov@users.noreply.github.com>
2016-12-10 02:00:16 +0200

    Changed weather report to use hbs template

--- src/js/lib/renderers/weatherReport.js
+++ src/js/lib/renderers/weatherReport.js
@@ -1,58 +1,17 @@
 
+var weatherReport = require('../templates/weatherReport.hbs');
 var $article = $('#weather-today');
 
 
-function _createLocationHTML (weather) {
-  var $locHTML = $('<h2></h2>',
-      {'class': 'text-center'}).text(' ' + weather.getTown() + ', ');
-  $locHTML.append($('<small></small>').text(weather.getCountryCode()));
-  $locHTML.prepend($('<i></i>',
-      {'class': weather.getWeatherIconsClass()}));
-  return $locHTML;
-}
-
-function _createWeatherInfoHTML (weather) {
-  var $holder = $('<section></section>', {'class': 'temp text-center'});
-  var $temp = $('<span></span>',
-      {'class': 'temp-val', id: 'temp-val'}).text(weather.getTemp().C);
-  var $deg = _createC2FSwitchHTML(weather);
-  $holder.append($('<i></i>', {'class': 'wi wi-thermometer temp-icon'}));
-  $holder.append($temp);
-  $holder.append($deg);
-  return $holder;
-}
-
-function _createC2FSwitchHTML (weather) {
-  var temps = weather.getTemp();
-  var $btnGrp = $('<div></div>',
-      {'class': 'btn-group', role: 'group'});
-  var $cBtn = $('<button></button>',
-      {'class': 'btn btn-primary', type: 'button', id: 'c-btn'})
-      .text('C');
-  var $fBtn = $('<button></button>',
-      {'class': 'btn btn-default', type: 'button', id: 'f-btn'})
-      .text('F');
-  $cBtn.data({'temp-val': temps.C, 'other-btn': '#f-btn'});
-  $fBtn.data({'temp-val': temps.F, 'other-btn': '#c-btn'});
-  $btnGrp.append($cBtn);
-  $btnGrp.append($fBtn);
-  return $btnGrp;
-}
-
-function _createWeatherDescriptionHTML (weather) {
-  var $desc = $('<p></p>',
-      {'class': 'text-center small weather-description'});
-  $desc.text(weather.getWeatherDescription());
-  return $desc;
-}
-
 function addWeatherHTML (weather) {
-  var $locationHTML = _createLocationHTML(weather);
-  var $weatherInfoHTML = _createWeatherInfoHTML(weather);
-  var $weatherDescription = _createWeatherDescriptionHTML(weather);
-  $article.html($locationHTML);
-  $article.append($weatherDescription);
-  $article.append($weatherInfoHTML);
+  var data = {
+    town: weather.getTown(),
+    countryCode: weather.getCountryCode(),
+    weatherDescription: weather.getWeatherDescription(),
+    temp: weather.getTemp(),
+    weatherIconClass: weather.getWeatherIconsClass()
+  };
+  $article.html(weatherReport(data));
 }
 
 
